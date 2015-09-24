b93915d539b684ee087e8a1cbc8350abbef61a24
Harald Kirschner <hkirschner@mozilla.com>
2015-09-23 21:08:09 -0700

    Added travis and build steps for gh-pages and heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
