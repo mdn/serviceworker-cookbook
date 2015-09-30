e36baa94855a7a0b6d245f147ddd2df892e89aab
Harald Kirschner <hkirschner@mozilla.com>
2015-09-29 17:18:47 -0700

    Force SSL with HSTS headers

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const app = express();
 
 app.use(function forceSSL(req, res, next) {
   const host = req.get('Host');
-  if (!host.startsWith('localhost') && req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect('https://' + host + req.url);
+  if (!host.startsWith('localhost') {
+    // https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
+    res.header('Strict-Transport-Security', 'max-age=15768000');
+    //https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + host + req.url);
+    }
   }
   return next();
 });
 
-// http://enable-cors.org/server_expressjs.html
 app.use(function corsify(req, res, next) {
+  // http://enable-cors.org/server_expressjs.html
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
   next();
