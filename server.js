805badc3b6f3be24a380fdc75178ac84e04ce4b4
Harald Kirschner <hkirschner@mozilla.com>
2015-09-30 09:44:38 -0700

    Fix typo

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(function forceSSL(req, res, next) {
   const host = req.get('Host');
-  if (!host.startsWith('localhost') {
+  if (!host.startsWith('localhost')) {
     // https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
     res.header('Strict-Transport-Security', 'max-age=15768000');
-    //https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
+    // https://github.com/rangle/force-ssl-heroku/blob/master/force-ssl-heroku.js
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect('https://' + host + req.url);
     }
