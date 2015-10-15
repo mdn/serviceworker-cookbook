683122894cf9cbaa27ee49ea903232d5ea989953
franciov <f.iovine@gmail.com>
2015-10-15 17:55:43 +0100

    now adopting a subset of ES6: no class

--- live-flowchart/app.js
+++ live-flowchart/app.js
@@ -1,122 +1,119 @@
-'use strict';
-
 /* global Logger, SWUtil */
-
-// import Logger from 'logger';
-// import SWUtil from 'sw-util';
 
 /*
  * @class App
  * This application
  */
-class App {
-
-  /*
-   * @constructs
-   */
-  constructor() {
-    Logger.log('App()');
-
-    // instatiate a new Service Worker Utility Class
-    this.swUtil = new SWUtil();
-
-    // register click events
-    document.querySelector('#reloadapp').addEventListener('click', () => {
-      window.location.reload();
-    });
-
-    // checking whether service workers are supported
-    if (this.swUtil.areServiceWorkersSupported()) {
-      document.querySelector('#swinstall').addEventListener('click', () => {
-        Logger.log('\n-------\n');
-        thisApp.enableCoolFeatures();
-      });
-
-      document.querySelector('#swuninstall').addEventListener('click', () => {
-        Logger.log('\n-------\n');
-        thisApp.disableCoolFeatures();
-      });
-
-      // checking whether a service worker is in control
-      if (this.swUtil.isServiceWorkerControllingThisApp()) {
-        Logger.info('App code run as expected');
-
-        this.disableServiceWorkerRegistration();
-      } else {
-        this.enableServiceWorkerRegistration();
-      }
-    } else {
-      Logger.error('Service workers are not supported by this browser');
-    }
-  }
-
-  /*
-   * @method enableCoolFeatures
-   * Try register a service worker in order to enable cool features (e.g. offline navigation)
-   */
-  enableCoolFeatures() {
-    var scriptURL;
-    var scope;
-
-    Logger.log('\nApp.enableCoolFeatures()');
-
-    // get params from DOM inputs
-    scriptURL = document.querySelector('#swscripturl');
-    scope = document.querySelector('#swscope');
-
-    Logger.debug('Configuring the following service worker ' + scriptURL.value + ' with scope ' + scope.value);
-
-    if (scriptURL.value !== '') {
-      Logger.debug('scriptURL: ' + scriptURL.value);
-    } else {
-      Logger.error('No SW scriptURL specified');
-      return;
-    }
-
-    if (scope.value !== '') {
-      Logger.debug('scope: ' + scope.value);
-    } else {
-      Logger.warn('scope: not specified (scope defaults to the path the script sits in)');
-    }
-
-    // register the specified service worker
-    this.swUtil.registerServiceWorker(scriptURL.value, scope.value).then(
-        this.disableServiceWorkerRegistration, // success
-        this.enableServiceWorkerRegistration // error
-    );
-  }
-
-  /*
-   * @method disableCoolFeatures
-   * Try unregister the active service worker in order to disable cool features (e.g. offline navigation)
-   */
-  disableCoolFeatures() {
-    Logger.log('\nApp.disableCoolFeatures()');
-
-    this.swUtil.unregisterServiceWorker().then(
-        this.enableServiceWorkerRegistration, // success
-        this.disableServiceWorkerRegistration // error
-    );
-  }
-
-  /*
-   * @method enableServiceWorkerRegistration
-   * Enable the possibility for the user to register a service worker
-   */
-  enableServiceWorkerRegistration() {
-    document.querySelector('#swinstall').disabled = false;
-    document.querySelector('#swuninstall').disabled = true;
-  }
-
-  /*
-   * @method disableServiceWorkerRegistration
-   * Disable the possibility for the user to unregister a service worker
-   */
-  disableServiceWorkerRegistration() {
-    document.querySelector('#swinstall').disabled = true;
-    document.querySelector('#swuninstall').disabled = false;
-  }
+function App() {
+  this.constructor();
 }
 
+/*
+ * @constructs
+ */
+App.prototype.constructor = function constructor() {
+  Logger.log('new App()');
+
+  // instatiate a new Service Worker Utility Class
+  this.swUtil = new SWUtil();
+
+  // register click events
+  document.querySelector('#reloadapp').addEventListener('click', () => {
+    window.location.reload();
+  });
+
+  // checking whether service workers are supported
+  if (this.swUtil.areServiceWorkersSupported()) {
+    document.querySelector('#swinstall').addEventListener('click', () => {
+      Logger.log('\n-------\n');
+      this.enableCoolFeatures();
+    });
+
+    document.querySelector('#swuninstall').addEventListener('click', () => {
+      Logger.log('\n-------\n');
+      this.disableCoolFeatures();
+    });
+
+    // checking whether a service worker is in control
+    if (this.swUtil.isServiceWorkerControllingThisApp()) {
+      Logger.info('App code run as expected');
+
+      this.disableServiceWorkerRegistration();
+    } else {
+      this.enableServiceWorkerRegistration();
+    }
+  } else {
+    Logger.error('Service workers are not supported by this browser');
+  }
+};
+
+/*
+ * @method enableCoolFeatures
+ * Try register a service worker in order to enable cool features (e.g. offline navigation)
+ */
+App.prototype.enableCoolFeatures = function enableCoolFeatures() {
+  var scriptURL;
+  var scope;
+
+  Logger.log('\nApp.enableCoolFeatures()');
+
+  // get params from DOM inputs
+  scriptURL = document.querySelector('#swscripturl');
+  scope = document.querySelector('#swscope');
+
+  Logger.debug('Configuring the following service worker ' + scriptURL.value + ' with scope ' + scope.value);
+
+  if (scriptURL.value !== '') {
+    Logger.debug('scriptURL: ' + scriptURL.value);
+  } else {
+    Logger.error('No SW scriptURL specified');
+    return;
+  }
+
+  if (scope.value !== '') {
+    Logger.debug('scope: ' + scope.value);
+  } else {
+    Logger.warn('scope: not specified (scope defaults to the path the script sits in)');
+  }
+
+  // register the specified service worker
+  this.swUtil.registerServiceWorker(scriptURL.value, scope.value).then(
+      this.disableServiceWorkerRegistration, // success
+      this.enableServiceWorkerRegistration // error
+  );
+};
+
+/*
+ * @method disableCoolFeatures
+ * Try unregister the active service worker in order to disable cool features (e.g. offline navigation)
+ */
+App.prototype.disableCoolFeatures = function disableCoolFeatures() {
+  Logger.log('\nApp.disableCoolFeatures()');
+
+  this.swUtil.unregisterServiceWorker().then(
+      this.enableServiceWorkerRegistration, // success
+      this.disableServiceWorkerRegistration // error
+  );
+};
+
+/*
+ * @method enableServiceWorkerRegistration
+ * Enable the possibility for the user to register a service worker
+ */
+App.prototype.enableServiceWorkerRegistration = function enableServiceWorkerRegistration() {
+  document.querySelector('#swinstall').disabled = false;
+  document.querySelector('#swuninstall').disabled = true;
+};
+
+/*
+ * @method disableServiceWorkerRegistration
+ * Disable the possibility for the user to unregister a service worker
+ */
+App.prototype.disableServiceWorkerRegistration = function disableServiceWorkerRegistration() {
+  document.querySelector('#swinstall').disabled = true;
+  document.querySelector('#swuninstall').disabled = false;
+};
+
+
 // starts the application
-const thisApp = new App();
+new App();
--- live-flowchart/sw-util.js
+++ live-flowchart/sw-util.js
@@ -1,180 +1,178 @@
-'use strict';
-
 /* global Logger */
-
-// import Logger from 'logger';
 
 /*
  * @class SWUtil
  * Service Worker helper
  */
-class SWUtil {
-
-  /*
-   * @constructs
-   */
-  constructor() {
-    Logger.log('\nSWUtil()');
-  }
-
-  /*
-   * @method areServiceWorkersSupported
-   * Check whether service workers are supported
-   * @returns {Boolean}
-   */
-  areServiceWorkersSupported() {
-    Logger.log('\nSWUtil.areServiceWorkersSupported()');
-
-    Logger.debug('checking navigator.serviceWorker');
-
-    if (navigator.serviceWorker) {
-      Logger.info('Service workers supported by this browser');
-      return true;
-    }
-
-    Logger.warn('No, service workers are NOT supported by this browser');
-    Logger.debug(navigator.userAgent);
-    return false;
-  }
-
-  /*
-   * @method isServiceWorkerControllingThisApp
-   * Check whether a service worker is controlling the application
-   * @returns {Boolean}
-   */
-  isServiceWorkerControllingThisApp() {
-    Logger.log('\nSWUtil.isServiceWorkerControllingThisApp()');
-
-    Logger.debug('checking navigator.serviceWorker.controller');
-
-    if (navigator.serviceWorker.controller) {
-      Logger.info('A service worker controls this app');
-      Logger.debug('The following service worker controls this app: ');
-      Logger.debug(navigator.serviceWorker.controller.scriptURL + ' <= navigator.serviceWorker.controller.scriptURL');
-      Logger.info('Please enable and check the browser logs for the oninstall, onactivate, and onfetch events');
-      Logger.debug('More on about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
-
-      return true;
-    }
-
-    Logger.warn('No, there is no service worker controlling this app');
-    return false;
-  }
-
-  /*
-   * @method registerServiceWorker
-   * Register a service worker
-   * @param {String} path The script URL of the service worker to be registered
-   * @param {String} scope (optional) The scope of the service worker to be registered
-   * @returns {Promise}
-   */
-  registerServiceWorker(scriptURL, scope) {
-    var swRegisterSecondParam = {};
-
-    Logger.log('\nSWUtil.registerServiceWorker()');
-
-    Logger.info('Register service worker with serviceWorker.register()');
-
-    Logger.debug('navigator.serviceWorker.register(scriptURL, scope)');
-    Logger.debug('scriptURL: ' + scriptURL);
-
-    // add optional parameter 'scope' if specified
-    if (scope) {
-      swRegisterSecondParam.scope = scope;
-      Logger.debug('options.scope: ' + swRegisterSecondParam.scope);
-    }
-
-    // return a promise
-    return new Promise(
-      (resolve, reject) => {
-        navigator.serviceWorker.register(
-          scriptURL,
-          swRegisterSecondParam
-        ).then(
-          (swRegistration) => {
-            Logger.debug('success(swRegistration)');
-            Logger.debug(swRegistration);
-
-            if (swRegistration.active) {
-              Logger.debug('Registering the active service worker again: ');
-              Logger.debug(swRegistration.active.scriptURL + ' <= swRegistration.active.scriptURL');
-            }
-
-            if (swRegistration.installing) {
-              Logger.debug('Registering the following service worker for the first time: ');
-              Logger.debug(swRegistration.installing.scriptURL + ' <= swRegistration.installing.scriptURL');
-            }
-
-            if (swRegistration.scope) {
-              Logger.debug('with scope: ' + swRegistration.scope + ' <= swRegistration.scope');
-            }
-
-            Logger.info('Service worker registered');
-            Logger.info('Please enable and check the browser logs for the oninstall, onactivate, and onfetch events');
-            Logger.info('SW is in control, once document is reloaded');
-            Logger.debug('More on about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
-
-            resolve();
-          },
-          (why) => {
-            Logger.debug('error(why)');
-            Logger.error(why);
-
-            reject();
-          }
-        );
-      }
-    );
-  }
-
-  /*
-   * @method unregisterServiceWorker
-   * Unregister the active service worker
-   * @returns {Promise}
-   */
-  unregisterServiceWorker() {
-    Logger.log('\nSWUtil.unregisterServiceWorker()');
-
-    Logger.debug('Unregistering the active service worker...');
-    Logger.debug('if something goes wrong, please unregister the service worker ');
-    Logger.debug('about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
-
-    // return a promise
-    return new Promise(
-      (resolve, reject) => {
-        Logger.debug('navigator.serviceWorker.getRegistration().then(success, error)');
-        navigator.serviceWorker.getRegistration().then(
-          (swRegistration) => {
-            Logger.debug('success(swRegistration)');
-            Logger.debug(swRegistration);
-            Logger.debug('Unregistering the following service worker: ' + swRegistration.active.scriptURL + ' <= swRegistration.active.scriptURL');
-            Logger.debug('with scope ' + swRegistration.scope + ' <= swRegistration.scope');
-
-            Logger.debug('swRegistration.unregister().then()');
-            swRegistration.unregister()
-              .then(
-                () => {
-                  Logger.debug('success()');
-                  Logger.info('The service worker has been successfully unregistered');
-                  Logger.debug('More on about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
-
-                  resolve();
-                },
-                (why) => {
-                  Logger.debug('error(why)');
-                  Logger.error(why);
-
-                  reject();
-                }
-              );
-          },
-          (why) => {
-            Logger.debug('error(why)');
-            Logger.error(why);
-            Logger.warn('It has been not possibile to unregister the service worker');
-          }
-        );
-      }
-    );
-  }
+// class SWUtil {
+function SWUtil() {
+  this.constructor();
 }
+
+/*
+ * @constructs
+ */
+SWUtil.prototype.constructor = function constructor() {
+  Logger.log('new SWUtil()');
+};
+
+/*
+ * @method areServiceWorkersSupported
+ * Check whether service workers are supported
+ * @returns {Boolean}
+ */
+SWUtil.prototype.areServiceWorkersSupported = function areServiceWorkersSupported() {
+  Logger.log('\nSWUtil.areServiceWorkersSupported()');
+
+  Logger.debug('checking navigator.serviceWorker');
+
+  if (navigator.serviceWorker) {
+    Logger.info('Service workers supported by this browser');
+    return true;
+  }
+
+  Logger.warn('No, service workers are NOT supported by this browser');
+  Logger.debug(navigator.userAgent);
+  return false;
+},
+
+/*
+ * @method isServiceWorkerControllingThisApp
+ * Check whether a service worker is controlling the application
+ * @returns {Boolean}
+ */
+SWUtil.prototype.isServiceWorkerControllingThisApp = function isServiceWorkerControllingThisApp() {
+  Logger.log('\nSWUtil.isServiceWorkerControllingThisApp()');
+
+  Logger.debug('checking navigator.serviceWorker.controller');
+
+  if (navigator.serviceWorker.controller) {
+    Logger.info('A service worker controls this app');
+    Logger.debug('The following service worker controls this app: ');
+    Logger.debug(navigator.serviceWorker.controller.scriptURL + ' <= navigator.serviceWorker.controller.scriptURL');
+    Logger.info('Please enable and check the browser logs for the oninstall, onactivate, and onfetch events');
+    Logger.debug('More on about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
+
+    return true;
+  }
+
+  Logger.warn('No, there is no service worker controlling this app');
+  return false;
+};
+
+/*
+ * @method registerServiceWorker
+ * Register a service worker
+ * @param {String} path The script URL of the service worker to be registered
+ * @param {String} scope (optional) The scope of the service worker to be registered
+ * @returns {Promise}
+ */
+SWUtil.prototype.registerServiceWorker = function registerServiceWorker(scriptURL, scope) {
+  var swRegisterSecondParam = {};
+
+  Logger.log('\nSWUtil.registerServiceWorker()');
+
+  Logger.info('Register service worker with serviceWorker.register()');
+
+  Logger.debug('navigator.serviceWorker.register(scriptURL, scope)');
+  Logger.debug('scriptURL: ' + scriptURL);
+
+  // add optional parameter 'scope' if specified
+  if (scope) {
+    swRegisterSecondParam.scope = scope;
+    Logger.debug('options.scope: ' + swRegisterSecondParam.scope);
+  }
+
+  // return a promise
+  return new Promise(
+    (resolve, reject) => {
+      navigator.serviceWorker.register(
+        scriptURL,
+        swRegisterSecondParam
+      ).then(
+        (swRegistration) => {
+          Logger.debug('success(swRegistration)');
+          Logger.debug(swRegistration);
+
+          if (swRegistration.active) {
+            Logger.debug('Registering the active service worker again: ');
+            Logger.debug(swRegistration.active.scriptURL + ' <= swRegistration.active.scriptURL');
+          }
+
+          if (swRegistration.installing) {
+            Logger.debug('Registering the following service worker for the first time: ');
+            Logger.debug(swRegistration.installing.scriptURL + ' <= swRegistration.installing.scriptURL');
+          }
+
+          if (swRegistration.scope) {
+            Logger.debug('with scope: ' + swRegistration.scope + ' <= swRegistration.scope');
+          }
+
+          Logger.info('Service worker registered');
+          Logger.info('Please enable and check the browser logs for the oninstall, onactivate, and onfetch events');
+          Logger.info('SW is in control, once document is reloaded');
+          Logger.debug('More on about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
+
+          resolve();
+        },
+        (why) => {
+          Logger.debug('error(why)');
+          Logger.error(why);
+
+          reject();
+        }
+      );
+    }
+  );
+};
+
+/*
+ * @method unregisterServiceWorker
+ * Unregister the active service worker
+ * @returns {Promise}
+ */
+SWUtil.prototype.unregisterServiceWorker = function unregisterServiceWorker() {
+  Logger.log('\nSWUtil.unregisterServiceWorker()');
+
+  Logger.debug('Unregistering the active service worker...');
+  Logger.debug('if something goes wrong, please unregister the service worker ');
+  Logger.debug('about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
+
+  // return a promise
+  return new Promise(
+    (resolve, reject) => {
+      Logger.debug('navigator.serviceWorker.getRegistration().then(success, error)');
+      navigator.serviceWorker.getRegistration().then(
+        (swRegistration) => {
+          Logger.debug('success(swRegistration)');
+          Logger.debug(swRegistration);
+          Logger.debug('Unregistering the following service worker: ' + swRegistration.active.scriptURL + ' <= swRegistration.active.scriptURL');
+          Logger.debug('with scope ' + swRegistration.scope + ' <= swRegistration.scope');
+
+          Logger.debug('swRegistration.unregister().then()');
+          swRegistration.unregister()
+            .then(
+              () => {
+                Logger.debug('success()');
+                Logger.info('The service worker has been successfully unregistered');
+                Logger.debug('More on about:serviceworkers (Firefox) or chrome://serviceworker-internals/');
+
+                resolve();
+              },
+              (why) => {
+                Logger.debug('error(why)');
+                Logger.error(why);
+
+                reject();
+              }
+            );
+        },
+        (why) => {
+          Logger.debug('error(why)');
+          Logger.error(why);
+          Logger.warn('It has been not possibile to unregister the service worker');
+        }
+      );
+    }
+  );
+};
