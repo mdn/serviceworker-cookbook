b42a025c673f90e8eb6c0084972717416d256011
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-04 13:33:43 +0000

    Remove tag since it isn't useful for this recipe

--- push-simple/service-worker.js
+++ push-simple/service-worker.js
@@ -1,6 +1,5 @@
 self.addEventListener('push', function(event) {
   event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
     body: 'Alea iacta est',
-    tag: 'push',
   }));
 });
