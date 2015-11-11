5affcf46213928ab1f37f882264fd35b5c4838bf
Tim Abraldes <tim@timabraldes.com>
2015-11-10 17:23:43 -0800

    Updated WIP. Lots of TODOs

--- update-css/main.js
+++ update-css/main.js
@@ -1,4 +1,15 @@
-// TODO: Create a message handler for dealing with messages coming from
-// the Service Worker
+// TODO: Pressing the button on the page should update localstorage
+// info about which CSS file to consider 'up-to-date' and initiate
+// a countdown timer that will refresh the page with a description
+// of why and what to expect on the refreshed page.
+//
+// TODO: Message handler to respond to service worker when it asks us
+// what CSS file is 'up-to-date'
+//
+// TODO: Upon loading the page, the sw will ask the server for an updated
+// CSS file (in reality we will respond with localstorage data). If the sw
+// finds an udpated CSS file, it will inform us through a postmessage call.
+// We need to have a message handler that informs the user of the situation
+// and optionally refreshes the page.
 
 window.navigator.serviceWorker.register('sw.js');
--- update-css/sw.js
+++ update-css/sw.js
@@ -3,7 +3,8 @@ var cacheName = 'update-css-2015.1015.1202';
 self.addEventListener('install', function(ev) {
   self.skipWaiting();
 
-  // TODO: Add style-1.css to the cache
+  // TODO: Cache style-1.css as style.css
+  // TODO: Comment explaining what would happen here in a real site
 });
 
 self.addEventListener('activate', function(ev) {
@@ -15,9 +16,12 @@ self.addEventListener('activate', function(ev) {
 self.addEventListener('fetch', function(ev) {
   /*
    * TODO:
-   *   - initiate network fetch
-   *     - when fetch completes postMessage to the main page
-   *     - main page reloads or pops up UI
+   *   - Do the following only for 'style.css'
+   *   - postmessage asking for current CSS file
+   *   - initiate network fetch for current CSS file
+   *     - when fetch completes check whether it's different from what's already in the cache
+   *       - if no, done
+   *       - if yes, postMessage to page telling it that new CSS is available
    *   - return cached info
    */
 });
