6634429554bd6b6d22571e086fd25da94bdc8fbe
Tim Abraldes <tim@timabraldes.com>
2015-11-13 18:31:21 -0800

    Demo is now working in Chrome. Fails in FF

--- update-css/main.js
+++ update-css/main.js
@@ -1,15 +1,53 @@
-// TODO: Pressing the button on the page should update localstorage
-// info about which CSS file to consider 'up-to-date' and initiate
-// a countdown timer that will refresh the page with a description
-// of why and what to expect on the refreshed page.
-//
-// TODO: Message handler to respond to service worker when it asks us
-// what CSS file is 'up-to-date'
-//
-// TODO: Upon loading the page, the browser will fetch the CSS file. The
-// SW will intercept the call, ask us what CSS file it should respond with,
-// and initiate a network fetch. When the fetch completes, the SW will tell
-// us (if the file was different) that updated CSS is available. We need
-// a message listener that handles the SW telling us about updated CSS.
+var cacheName = 'update-css-2015.1015.1202';
 
 window.navigator.serviceWorker.register('sw.js');
+
+window.addEventListener('message', function(ev) {
+  console.log('MESSAGE ' + ev.data.msg);
+
+  if (!ev.data) {
+    return;
+  }
+
+  if (ev.data.msg === 'toggleCSSFilename') {
+    document.querySelector('#css-status').textContent = 'The next time this page is loaded, the SW will discover updated CSS on the server after serving the cached CSS';
+    return;
+  }
+});
+
+navigator.serviceWorker.addEventListener('message', function(ev) {
+  if (ev.data.msg === 'cssUpdated') {
+    console.log('GOT CSS UPDATED');
+    if (ev.data.val) {
+      document.querySelector('#new-css-msg').textContent = 'The server has a new CSS file available! If you refresh the page you should see updated CSS. In production, a site might decide to automatically refresh at this point';
+    } else {
+      document.querySelector('#new-css-msg').textContent = 'The latest CSS has been served';
+    }
+
+    return;
+  }
+});
+
+var updateCSSButton = document.querySelector('#updateCSSButton');
+updateCSSButton.addEventListener('click', function(ev) {
+  updateCSSButton.disabled = true;
+  caches.open(cacheName).then(function(cache) {
+    cache.match('current-css-filename').then(function(response) {
+      var newValue;
+      if (response.text) {
+        response.text().then(function(value) {
+          if (value === 'style-1.css') {
+            newValue = 'style-2.css';
+          } else {
+            newValue = 'style-1.css';
+          }
+          cache.put('current-css-filename', new Response(newValue, { 'status': 200 }));
+          return window.postMessage({ msg: 'toggleCSSFilename' }, '*');
+        });
+      } else {
+        cache.put('current-css-filename', new Response('style-2.css', { 'status': 200 }));
+        return window.postMessage({ msg: 'toggleCSSFilename' }, '*');
+      }
+    });
+  });
+});
--- update-css/sw.js
+++ update-css/sw.js
@@ -3,8 +3,22 @@ var cacheName = 'update-css-2015.1015.1202';
 self.addEventListener('install', function(ev) {
   self.skipWaiting();
 
-  // TODO: Cache style-1.css as style.css
   // TODO: Comment explaining what would happen here in a real site
+
+  ev.waitUntil(caches.open(cacheName).then(function(cache) {
+    return cache.addAll([
+        'style-1.css',
+        'style-2.css',
+    ]).then(getCurrentCSSFilename).then(function(filename) {
+      // Cache whichever CSS file we currently want to serve as
+      // 'style.css'
+      return cache.match(filename).then(function(response) {
+        if (response) {
+          cache.put('style.css', response);
+        }
+      });
+    });
+  }));
 });
 
 self.addEventListener('activate', function(ev) {
@@ -14,14 +28,82 @@ self.addEventListener('activate', function(ev) {
 });
 
 self.addEventListener('fetch', function(ev) {
-  /*
-   * TODO:
-   *   - Do the following only for 'style.css'
-   *   - postmessage asking for current CSS file
-   *   - initiate network fetch for current CSS file
-   *     - when fetch completes check whether it's different from what's already in the cache
-   *       - if no, done
-   *       - if yes, postMessage to page telling it that new CSS is available
-   *   - return cached info
-   */
+  // Only process fetch requests for 'style.css'. Let everything
+  // else be handled normally.
+  if (ev.request.url.lastIndexOf('style.css') === -1) {
+    return;
+  }
+
+  // Respond with the cached version of 'style.css' after initiating
+  // a network fetch to update our cache if necessary.
+  ev.respondWith(caches.open(cacheName).then(function(cache) {
+    // Perform a network fetch of 'style.css' to see if there's an updated
+    // version available. If there is, replace our cached version with the
+    // updated one and use `postMessage` to notify the page that there
+    // is updated CSS available.
+    simulateStyleCSSNetworkFetch().then(function(response) {
+      // In real production code, we would use smarter heuristics to
+      // determine if the CSS file that we requested has actually been
+      // updated.
+      if (response && response.text) {
+        var clone = response.clone();
+        clone.text().then(function(networkText) {
+          cache.match('style.css').then(function(cachedResponse) {
+            if (cachedResponse && cachedResponse.text) {
+              cachedResponse.text().then(function(cacheText) {
+                if (cacheText !== networkText) {
+                  cache.put('style.css', response);
+                  // Inform any open pages that a new CSS file is available on
+                  // the server
+                  notifyPageOfCSSUpdate(true);
+                } else {
+                  notifyPageOfCSSUpdate(false);
+                }
+              });
+            } else {
+              notifyPageOfCSSUpdate(false);
+            }
+          });
+        });
+      } else {
+        notifyPageOfCSSUpdate(false);
+      }
+    });
+
+    return cache.match('style.css');
+  }));
 });
+
+function notifyPageOfCSSUpdate(isUpdated) {
+  console.log('NOTIFYING PAGE OF CSS UPDATE');
+  self.clients.matchAll().then(function(clientList) {
+    clientList.forEach(function(client) {
+      client.postMessage({ msg: 'cssUpdated', val: isUpdated });
+    });
+  });
+}
+
+function simulateStyleCSSNetworkFetch() {
+  return getCurrentCSSFilename().then(function(filename) {
+    return caches.open(cacheName).then(function(cache) {
+      // return the specified CSS file from the cache
+      return cache.match(filename);
+    });
+  });
+}
+
+function getCurrentCSSFilename() {
+  return caches.open(cacheName).then(function(cache) {
+    return cache.match('current-css-filename').then(function(cached) {
+      if (cached.text) {
+        return cached.text().then(function(text) {
+          return text;
+        });
+      }
+
+      cache.put('current-css-filename', new Response('style-1.css', { 'status': 200 }));
+      return 'style-1.css';
+    });
+  });
+}
+
