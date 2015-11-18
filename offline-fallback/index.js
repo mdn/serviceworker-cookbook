c83b6733952fe5ae8ad3679c65893d6cb48665e8
David Walsh <davidwalsh83@gmail.com>
2015-11-18 13:45:45 -0600

    Make offline-fallback replay-able, make it more clear what's happening

--- offline-fallback/index.js
+++ offline-fallback/index.js
@@ -2,12 +2,14 @@ if (navigator.serviceWorker.controller) {
   // A ServiceWorker controls the site on load and therefor can handle offline
   // fallbacks.
   debug(navigator.serviceWorker.controller.scriptURL + ' (onload)', 'controller');
+  debug('An active service worker controller was found, no need to register');
 } else {
   // Register the ServiceWorker
   navigator.serviceWorker.register('service-worker.js', {
     scope: './'
   }).then(function(reg) {
     debug(reg.scope, 'register');
+    debug('Service worker change, registered the service worker');
   });
 }
 
@@ -16,6 +18,14 @@ document.querySelector('#refresh').search = Date.now();
 
 // Debug helper
 function debug(message, element, append) {
-  var target = document.querySelector('#' + element || 'log');
+  var target = document.querySelector('#' + (element || 'log'));
   target.textContent = message + ((append) ? ('/n' + target.textContent) : '');
 }
+
+// Allow for "replaying" this example
+document.getElementById('clearAndReRegister').addEventListener('click', function() {
+  navigator.serviceWorker.getRegistration().then(function(registration) {
+    registration.unregister();
+    window.location.reload();
+  });
+});
