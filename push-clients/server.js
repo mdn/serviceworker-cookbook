6f4a8749791fb6bc2520b7fca5d397ab83f5a45f
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-18 00:23:54 +0000

    Add more detailed comments to the push-clients recipe

--- push-clients/server.js
+++ push-clients/server.js
@@ -1,3 +1,7 @@
+// Use the web-push library to hide the implementation details of the communication
+// between the application server and the push service.
+// For details, see https://tools.ietf.org/html/draft-ietf-webpush-protocol-01 and
+// https://tools.ietf.org/html/draft-thomson-webpush-encryption-01.
 var webPush = require('web-push');
 
 webPush.setGCMAPIKey(process.env.GCM_API_KEY);
