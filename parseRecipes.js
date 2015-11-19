9a4f1c40adbe1361f7c4915c5bff97a5a277049c
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-19 18:10:52 +0000

    Order by category first and then by difficulty

--- parseRecipes.js
+++ parseRecipes.js
@@ -12,22 +12,26 @@ module.exports = function(recipeSlugs) {
     var name = tokens[0].text;
     var summary = tokens[1].text;
 
-    var difficultyIdx;
-    for (difficultyIdx = 0; difficultyIdx < tokens.length; difficultyIdx++) {
-      var token = tokens[difficultyIdx];
-      if (token.type === 'heading' && token.text === 'Difficulty') {
-        break;
+    function getMetadata(type) {
+      var i;
+      for (i = 0; i < tokens.length; i++) {
+        var token = tokens[i];
+        if (token.type === 'heading' && token.text === type) {
+          break;
+        }
       }
-    }
-    assert(difficultyIdx !== -1, recipe + ': README.md should contain a difficulty');
-    assert(difficultyIdx + 1 <= tokens.length, recipe + ': README.md should contain a difficulty');
+      assert(i !== -1, recipe + ': README.md should contain a ' + type);
+      assert(i + 1 <= tokens.length, recipe + ': README.md should contain a ' + type);
 
-    var difficulty;
-    if (tokens[difficultyIdx + 1].text === 'Advanced') {
+      return tokens[i + 1].text;
+    }
+
+    var difficulty = getMetadata('Difficulty')
+    if (difficulty === 'Advanced') {
       difficulty = 3;
-    } else if (tokens[difficultyIdx + 1].text === 'Intermediate') {
+    } else if (difficulty === 'Intermediate') {
       difficulty = 2;
-    } else if (tokens[difficultyIdx + 1].text === 'Beginner') {
+    } else if (difficulty === 'Beginner') {
       difficulty = 1;
     } else {
       assert(false, recipe + ': Unexpected difficulty value');
@@ -41,10 +45,12 @@ module.exports = function(recipeSlugs) {
         ref: recipe + '_' + srcName + '_doc.html'
       };
     });
+
     return {
       name: name,
       summary: summary,
       difficulty: difficulty,
+      category: getMetadata('Category'),
       slug: recipe,
       srcs: srcs,
       demo_ref: recipe + '_demo.html',
