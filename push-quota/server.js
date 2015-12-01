7e4e15742fda909a53841e2f087f7d41e5fb0a6d
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-01 18:16:53 +0000

    Add documentation for push-quota. Fixes #152

--- push-quota/server.js
+++ push-quota/server.js
@@ -1,3 +1,7 @@
+// Use the web-push library to hide the implementation details of the communication
+// between the application server and the push service.
+// For details, see https://tools.ietf.org/html/draft-ietf-webpush-protocol-01 and
+// https://tools.ietf.org/html/draft-thomson-webpush-encryption-01.
 var webPush = require('web-push');
 
 webPush.setGCMAPIKey(process.env.GCM_API_KEY);
@@ -11,6 +15,10 @@ module.exports = function(app, route) {
     key = req.body.key;
   });
 
+  // Send N notifications, specifying whether the service worker will need to show
+  // a visible notification or not using the push payload:
+  // - 'true': show a notification;
+  // - 'false': don't show a notification.
   app.post(route + 'sendNotification', function(req) {
     var num = 1;
 
