07c939287dffd66da593b0ad4b80f8eae6933107
Marco Castelluccio <mcastelluccio@mozilla.com>
2015-12-02 13:20:10 +0000

    Add inline comment explaining why on Chrome we don't know the sender of the message

--- message-relay/service-worker.js
+++ message-relay/service-worker.js
@@ -4,13 +4,19 @@ self.addEventListener('message', function(event) {
     // Get all the connected clients and forward the message along.
     self.clients.matchAll()
     .then(function(clientList) {
+      // event.source.id contains the ID of the sender of the message.
+      // `event` in Chrome isn't an ExtendableMessageEvent yet (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/#extendablemessage-event-interface),
+      // so it doesn't have the `source` property.
+      // https://code.google.com/p/chromium/issues/detail?id=543198
+      var senderID = event.source ? event.source.id : 'unknown';
+
       clientList.forEach(function(client) {
         // Skip sending the message to the client that sent it.
-        if (client.id === event.source.id) {
+        if (client.id === senderID) {
           return;
         }
         client.postMessage({
-          client: event.source.id,
+          client: senderID,
           message: event.data
         });
       });
