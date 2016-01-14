098393b39985adcebfca388590870b4f65367807
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-15 00:22:37 +0100

    Make all requests return a status code

--- push-payload/server.js
+++ push-payload/server.js
@@ -7,14 +7,18 @@ var webPush = require('web-push');
 webPush.setGCMAPIKey(process.env.GCM_API_KEY);
 
 module.exports = function(app, route) {
-  app.post(route + 'register', function() {
+  app.post(route + 'register', function(req, res) {
     // A real world application would store the subscription info.
+    res.sendStatus(201);
   });
 
-  app.post(route + 'sendNotification', function(req) {
+  app.post(route + 'sendNotification', function(req, res) {
     setTimeout(function() {
       webPush.sendNotification(req.body.endpoint, req.body.ttl, req.body.key,
-                               req.body.payload);
+                               req.body.payload)
+      .then(function() {
+        res.sendStatus(201);
+      });
     }, req.body.delay * 1000);
   });
 };
--- push-simple/server.js
+++ push-simple/server.js
@@ -7,13 +7,17 @@ var webPush = require('web-push');
 webPush.setGCMAPIKey(process.env.GCM_API_KEY);
 
 module.exports = function(app, route) {
-  app.post(route + 'register', function() {
+  app.post(route + 'register', function(req, res) {
     // A real world application would store the subscription info.
+    res.sendStatus(201);
   });
 
-  app.post(route + 'sendNotification', function(req) {
+  app.post(route + 'sendNotification', function(req, res) {
     setTimeout(function() {
-      webPush.sendNotification(req.query.endpoint, req.query.ttl);
+      webPush.sendNotification(req.query.endpoint, req.query.ttl)
+      .then(function() {
+        res.sendStatus(201);
+      });
     }, req.query.delay * 1000);
   });
 };
