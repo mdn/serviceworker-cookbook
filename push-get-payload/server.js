0929e16a3ea2819199d03c342a64547a8ad9e85e
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-20 16:38:00 +0000

    Add recipe that allows getting a payload without using PushMessageData. Fixes #198

---
+++ push-get-payload/server.js
@@ -0,0 +1,30 @@
+// Use the web-push library to hide the implementation details of the communication
+// between the application server and the push service.
+// For details, see https://tools.ietf.org/html/draft-ietf-webpush-protocol-01 and
+// https://tools.ietf.org/html/draft-thomson-webpush-encryption-01.
+var webPush = require('web-push');
+
+webPush.setGCMAPIKey(process.env.GCM_API_KEY);
+
+var payloads = {};
+
+module.exports = function(app, route) {
+  app.post(route + 'register', function(req, res) {
+    // A real world application would store the subscription info.
+    res.sendStatus(201);
+  });
+
+  app.post(route + 'sendNotification', function(req, res) {
+    setTimeout(function() {
+      payloads[req.body.endpoint] = req.body.payload;
+      webPush.sendNotification(req.body.endpoint, req.body.ttl)
+      .then(function() {
+        res.sendStatus(201);
+      });
+    }, req.body.delay * 1000);
+  });
+
+  app.get(route + 'getPayload', function(req, res) {
+    res.send(payloads[req.query.endpoint]);
+  });
+};
