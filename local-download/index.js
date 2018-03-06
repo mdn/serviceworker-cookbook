dba37c79dba26d186f790f0e4d6215a122f80d87
dmonego <dmonego@gmail.com>
2018-03-06 18:39:31 -0500

    Applied suggested updates

--- local-download/index.js
+++ local-download/index.js
@@ -1,10 +1,4 @@
 // Register the ServiceWorker
 navigator.serviceWorker.register('service-worker.js', {
   scope: '.'
-}).then(function(registration) {
-  console.log('The service worker has been registered ', registration);
-  var content = document.getElementById("text");
-  var button = document.getElementById("download");
-
-});
-
+})
