c34769a9fa6ce03ca320c6f103824d3723986687
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2018-04-08 14:29:41 +0200

    Run subscribe only when serviceWorker.ready resolves
    The register promise may resolve even when the serviceWorker is not ready,
    this makes sure it is ready to setup a subscription.

--- push-replace/index.js
+++ push-replace/index.js
@@ -1,5 +1,7 @@
 // Register a Service Worker.
-navigator.serviceWorker.register('service-worker.js')
+navigator.serviceWorker.register('service-worker.js');
+
+navigator.serviceWorker.ready
 .then(function(registration) {
   // Use the PushManager to get the user's subscription to the push service.
   return registration.pushManager.getSubscription()
